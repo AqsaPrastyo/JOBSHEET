6fbdc487ffbb180375690af7d44e09809cfdfdee
DrWallace <95905290+AqsaPrastyo@users.noreply.github.com>
2023-09-13 08:43:08 +0700

    Add files via upload

---
+++ src/P2/MyfirstJavaGitHub.java
@@ -0,0 +1,11 @@
+package src.P2;
+
+/**
+ * MyfirstJavaGitHub
+ */
+public class MyfirstJavaGitHub {
+
+   public static void main(String[] args) {
+     System.out.println("Hello form github ");
+   }
+}
---
+++ src/P3/PrakDaspro_1C_04.java
@@ -0,0 +1,15 @@
+package src.P3;
+
+public class PrakDaspro_1C_04 {
+  
+  public static void main(String[] args) {
+    
+System.out.println("Hello");
+System.out.println("wow");
+System.out.println("GG");
+
+
+
+
+  }
+}
