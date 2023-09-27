9399121b0567c35a8f236194d357beba1f762f44
Herry <aqsaherryprasetya@gmail.com>
2023-09-27 09:31:48 +0700

    P5_Percobaan3_04

---
+++ src/P5/PemilihanPercobaan3.java
@@ -0,0 +1,43 @@
+package src.P5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan3 {
+  public static void main(String[] args) {
+    Scanner input04 = new Scanner(System.in);
+
+    double angka1, angka2, hasil;
+    char operator;
+
+    System.out.print("Masukan angka pertama: ");
+    angka1 = input04.nextDouble();
+    System.out.print("Masukan angka kedua: ");
+    angka2 = input04.nextDouble();
+    System.out.print("Masukan operator (+ - * /) : ");
+    operator = input04.next().charAt(0);
+
+    switch (operator) {
+      case '+' :
+        hasil = angka1 + angka2;
+      System.out.println(angka1  + " + " + angka2 + " = " + hasil);
+        break;
+      case '-' :
+        hasil = angka1 - angka2;
+      System.out.println(angka1  + " - " + angka2 + " = " + hasil);
+        break;
+      case '*' :
+        hasil = angka1 * angka2;
+      System.out.println(angka1  + " * " + angka2 + " = " + hasil);
+        break;
+      case '/' :
+        hasil = angka1 / angka2;
+      System.out.println(angka1  + " / " + angka2 + " = " + hasil);
+        break;
+    
+      default:
+      System.out.println("Operator yang Anda masukkan salah ");
+        break;
+    }
+
+  } 
+}
