ae1cf872e98ee7c8be91b3104f939f55319004cf
Herry <aqsaherryprasetya@gmail.com>
2023-09-27 08:42:48 +0700

    P5_Percobaan1_Pertayaan1_04

---
+++ src/P5/PemilihanPercobaan104.java
@@ -0,0 +1,28 @@
+package src.P5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan104 {
+  public static void main(String[] args) {
+    Scanner input04 = new Scanner(System.in);
+
+System.out.print("Masukan angka: ");
+int angka = input04.nextInt();
+String genap;
+
+
+// menggunakan if
+// if (angka %2==0 ) {
+//  System.out.println("Angka " + angka + " bilangan genap");
+// }else{
+//  System.out.println("Angka " + angka + " bilangan ganjil");
+// }
+
+//menggunakan Tenary Operator
+genap = (angka % 2==0) ?  "Genap" : "ganjil";
+
+System.out.println(genap);
+
+
+  }
+}
