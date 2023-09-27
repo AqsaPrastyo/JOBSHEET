9faa521812df57611885149ab516497798c38f94
Herry <aqsaherryprasetya@gmail.com>
2023-09-27 08:29:15 +0700

    P5_Percobaan1_04

---
+++ P5/PemilihanPercobaan104.java
@@ -0,0 +1,23 @@
+package P5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan104 {
+  public static void main(String[] args) {
+    Scanner input04 = new Scanner(System.in);
+
+System.out.print("Masukan angka: ");
+int angka = input04.nextInt();
+String genap;
+
+if (angka %2==0 ) {
+  System.out.println("Angka " + angka + " bilangan genap");
+}else{
+  System.out.println("Angka " + angka + " bilangan ganjil");
+}
+
+
+
+
+  }
+}
