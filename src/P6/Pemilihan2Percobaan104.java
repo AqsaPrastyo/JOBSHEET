970aa0065293ad88a7cedbe39bfdbc37aaaff7bd
Herry <aqsaherryprasetya@gmail.com>
2023-10-04 09:06:31 +0700

    P6_Percobaan1_04

---
+++ src/P6/Pemilihan2Percobaan104.java
@@ -0,0 +1,30 @@
+package src.P6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan104 {
+  public static void main(String[] args) {
+    
+Scanner input04 = new Scanner(System.in);
+
+System.out.print("Masukan tahun: ");
+int tahun = input04.nextInt();
+
+if (tahun % 4 == 0) {
+  if (tahun % 100 != 0) {
+    System.out.println("tahun kabisat");
+  } 
+} else {
+  System.out.println("Bukan tahun kabisat");
+}
+
+
+
+
+
+
+
+
+    
+  }
+}
