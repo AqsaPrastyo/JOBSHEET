65ceea2deb9504a8159affaee68301acf80598f7
Herry <aqsaherryprasetya@gmail.com>
2023-10-04 10:48:37 +0700

    P6_Pemilihan2_Percobaan3_04

---
+++ src/P6/Pemilihan2Percobaan3.java
@@ -0,0 +1,46 @@
+package src.P6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan3 {
+  public static void main(String[] args) {
+    Scanner input04 = new Scanner(System.in);
+
+    String kategori;
+    int penghasilan, gajiBersih;
+    double pajak;
+    
+    System.out.print("Masukan kategori: ");
+    kategori = input04.nextLine();
+    System.out.print("Masukan penghasilan: ");
+    penghasilan = input04.nextInt();
+    
+    if (kategori.equalsIgnoreCase("Pekerja")) {
+     if (penghasilan <= 2_000_000 ) {
+      pajak = 0.1;
+     }else if (penghasilan <= 3_000_000) {
+      pajak = 0.15;
+     } else {
+      pajak = 0.2;
+     }
+      
+    gajiBersih = (int) (penghasilan - (penghasilan * pajak));
+    System.out.println("Gaji yang anda dapatkan adalah: " + gajiBersih);
+    } else if (kategori.equalsIgnoreCase("Pebisnis")) {
+      if (penghasilan <= 2_500_000) {
+        pajak = 0.15;
+      } else if (penghasilan <= 3_500_00) {
+        pajak = 0.2;
+      }else {
+        pajak = 0.25;
+      }
+    
+    gajiBersih = (int) (penghasilan - (penghasilan * pajak));
+    System.out.println("Gaji yang anda dapatkan adalah: ");
+    }else {
+      System.out.println("Kategori yang anda masukan salah!");
+    }
+
+
+  }
+}
