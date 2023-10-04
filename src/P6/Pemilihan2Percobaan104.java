7ab63ada9de58e8818a8b0782af49909e9e12401
Herry <aqsaherryprasetya@gmail.com>
2023-10-04 09:36:32 +0700

    Percobaan1_pertayaan1_04

--- src/P6/Pemilihan2Percobaan104.java
+++ src/P6/Pemilihan2Percobaan104.java
@@ -13,7 +13,9 @@ int tahun = input04.nextInt();
 if (tahun % 4 == 0) {
   if (tahun % 100 != 0) {
     System.out.println("tahun kabisat");
-  } 
+  } else{
+    System.out.println("Bukan tahun kabisat");
+  }
 } else {
   System.out.println("Bukan tahun kabisat");
 }
