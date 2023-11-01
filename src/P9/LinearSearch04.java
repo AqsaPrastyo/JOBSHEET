701cb57d801a2c29f22d8ae5b9e189e88a473ca7
Herry <aqsaherryprasetya@gmail.com>
2023-11-01 11:42:53 +0700

    P9_Percobaan4_Pertanyaan2_04

--- src/P9/LinearSearch04.java
+++ src/P9/LinearSearch04.java
@@ -8,41 +8,52 @@ public class LinearSearch04 {
 
 
  // yang awal 
-int[] arrayint = {34,18,26,48,72,20,56,63};
-int key = 20;
-int hasil = 0;
+// int[] arrayint = {34,18,26,48,72,20,56,63};
+// int key = 20;
+// int hasil = 0;
 
 
-for (int i = 0; i < arrayint.length; i++) {
-  if (arrayint[i] == key) {
-    hasil = i;
-    break;
-  }
-}
-System.out.println("Key ada dalam array pada posisi indeks ke- "+hasil);
-
-
-  
-// System.out.print("Masukkan jumlah elemen array : ");
-//   int n = input04.nextInt();
-
-// int[] arrayint = new int[n];
-// for (int i = 0; i < arrayint.length; i++) {
-//   System.out.print("Masukan elemen array ke-" + (i + 1)+ " : ");
-//   arrayint[i] = input04.nextInt();  
-// }
-
-// System.out.print("Masukan key yang ingin anda cari : ");
-// int key = input04.nextInt();
-
-// int hasil =-1;
 // for (int i = 0; i < arrayint.length; i++) {
 //   if (arrayint[i] == key) {
 //     hasil = i;
 //     break;
 //   }
 // }
+// System.out.println("Key ada dalam array pada posisi indeks ke- "+hasil);
 
+
+  // Menjawab Pertanyaan
+System.out.print("Masukkan jumlah elemen array : ");
+  int n = input04.nextInt();
+
+int[] arrayint = new int[n];
+for (int i = 0; i < arrayint.length; i++) {
+  System.out.print("Masukan elemen array ke-" + (i + 1)+ " : ");
+  arrayint[i] = input04.nextInt();  
+}
+
+System.out.print("Masukan key yang ingin anda cari : ");
+int key = input04.nextInt();
+
+int hasil =0;
+for (int i = 0; i < arrayint.length; i++) {
+  if (arrayint[i] == key) {
+    hasil = i;
+    break;
+  }
+}
+
+//Untuk menjawab pertanyaan no 2
+if (arrayint[hasil]== key) {
+  System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+}else{
+  System.out.println("Key tidak ditemukan dalam array");
+}
+
+
+
+
+ //Untuk menjawab pertanyaan no 3
 // if (hasil !=-1) {
 //   System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
 // } else {
