ace34a6af5abad98bc8d2ffa465fae8d3727b20e
Herry <aqsaherryprasetya@gmail.com>
2023-11-01 08:50:04 +0700

    P9_Percobaan1_Pertayaan1_04

--- src/P9/ArrayBilanga04.java
+++ src/P9/ArrayBilanga04.java
@@ -5,10 +5,10 @@ public class ArrayBilanga04 {
     
     int[] bil = new int[4];
 
-    bil[0] = 5;
-    bil[1] = 13;
-    bil[2] = -7;
-    bil[3] = 17;
+    bil[0] = 5.0;
+    bil[1] = 12867;
+    bil[2] = 7.5;
+    bil[3] = 2000000;
 
   System.out.println(bil[0]);
   System.out.println(bil[1]);
