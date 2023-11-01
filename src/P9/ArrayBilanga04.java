8d44979c4c195bb395bbad7c74659c54a8a4d8eb
Herry <aqsaherryprasetya@gmail.com>
2023-11-01 09:02:32 +0700

    P9_Percobaan1_Pertayaan2_04

--- src/P9/ArrayBilanga04.java
+++ src/P9/ArrayBilanga04.java
@@ -3,17 +3,24 @@ package src.P9;
 public class ArrayBilanga04 {
   public static void main(String[] args) {
     
-    int[] bil = new int[4];
+    int[] bil = {5,13,-7,17};
+    
 
-    bil[0] = 5.0;
-    bil[1] = 12867;
-    bil[2] = 7.5;
-    bil[3] = 2000000;
+System.out.println(bil[1]);
 
-  System.out.println(bil[0]);
-  System.out.println(bil[1]);
-  System.out.println(bil[2]);
-  System.out.println(bil[3]);
+    // bil[0] = 5;
+    // bil[1] = 13;
+    // bil[2] = -7;
+    // bil[3] = 17;
+
+
+
+
+    
+  // System.out.println(bil[0]);
+  // System.out.println(bil[1]);
+  // System.out.println(bil[2]);
+  // System.out.println(bil[3]);
 
 
     
