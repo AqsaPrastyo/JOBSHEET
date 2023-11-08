63b5f22649f1abf8cc76ae9c6db4421515fd07b0
Herry <aqsaherryprasetya@gmail.com>
2023-11-08 08:36:42 +0700

    P10_Percobaan 1_04

---
+++ src/P10/Bioskop04.java
@@ -0,0 +1,31 @@
+package src.P10;
+
+public class Bioskop04 {
+  public static void main(String[] args) {
+    
+System.out.println("Hello world");
+
+String penonton [][] = new String[4][2];
+
+penonton [0][0] = "Amir"; 
+penonton [0][1] = "Benna";
+penonton [1][0] = "Candra"; 
+penonton [1][1] = "Dela"; 
+penonton [2][0] = "Eka";
+penonton [2][1] = "Farhan"; 
+penonton [3][0] = "Gisel"; 
+
+    System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]) ;
+    System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]) ;
+    System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]) ;
+    System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]) ;
+
+
+
+
+
+
+    
+
+  }
+}
