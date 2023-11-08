3a8d2d07f0f1382338c61befc55b285507d76fc3
Herry <aqsaherryprasetya@gmail.com>
2023-11-08 08:56:45 +0700

    P10_Percobaan1_Pertanyaan5_04

--- src/P10/Bioskop04.java
+++ src/P10/Bioskop04.java
@@ -24,13 +24,18 @@ penonton [3][1] = "Hannah";
 
 
 // untuk menjawab no 4
+// System.out.println(penonton.length);
+// System.out.println(penonton[0].length);
+// System.out.println(penonton[1].length);
+// System.out.println(penonton[2].length);
+// System.out.println(penonton[3].length);
+
+// Untuk menjawab no 5
 System.out.println(penonton.length);
-System.out.println(penonton[0].length);
-System.out.println(penonton[1].length);
-System.out.println(penonton[2].length);
-System.out.println(penonton[3].length);
 
-
+for (int i = 0; i < penonton.length; i++) {
+  System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+}
 
 
 
