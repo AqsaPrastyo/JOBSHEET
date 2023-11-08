0d68ef28caef3b4603727e917390cec19a759d39
Herry <aqsaherryprasetya@gmail.com>
2023-11-08 11:31:33 +0700

    P10_Percobaan3_Pertannyaan4_04

--- src/P10/Numbers04.java
+++ src/P10/Numbers04.java
@@ -1,5 +1,7 @@
 package src.P10;
 
+import java.util.Arrays;
+
 public class Numbers04 {
   public static void main(String[] args) {
     int [][] myNumbers04 = new int[3][];
@@ -9,11 +11,13 @@ myNumbers04 [2] = new int[1];
 
 // Untuk menjawab no 1
 for (int i = 0; i < myNumbers04.length; i++) {
-  System.out.println(Array.toString(myNumbers04[i]));
+  System.out.println(Arrays.toString(myNumbers04[i]));
 }
 
-
-
+// Untuk menjawab no 4
+for (int i = 0; i < myNumbers04.length; i++) {
+ System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers04[i].length); 
+}
 
 
 
