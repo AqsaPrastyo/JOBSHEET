03d3d55ba057a217df9297bf94b10dfa4559d63f
Herry <aqsaherryprasetya@gmail.com>
2023-11-08 11:24:28 +0700

    P10_Percobaan3_Pertannyaan1_04

--- src/P10/Numbers04.java
+++ src/P10/Numbers04.java
@@ -7,6 +7,20 @@ myNumbers04 [0] = new int[5];
 myNumbers04 [1] = new int[3];
 myNumbers04 [2] = new int[1];
 
+// Untuk menjawab no 1
+for (int i = 0; i < myNumbers04.length; i++) {
+  System.out.println(Array.toString(myNumbers04[i]));
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
