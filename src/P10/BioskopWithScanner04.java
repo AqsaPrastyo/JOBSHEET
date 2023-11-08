6123d88e7f4cc005231c9ef9ff5a1626710f11ae
Herry <aqsaherryprasetya@gmail.com>
2023-11-08 10:20:11 +0700

    P10_Percobaan2_Pertannyaan3_04

--- src/P10/BioskopWithScanner04.java
+++ src/P10/BioskopWithScanner04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BioskopWithScanner04 {
   public static void main(String[] args) {
     Scanner sc04 = new Scanner(System.in);
-
+    
     String penonton [][] = new String[4][2];
 
 // while (true) {
@@ -40,16 +40,24 @@ switch (pilihMenu) {
   case 1:
     while (true) {
   System.out.print("Masukan nama: ");
-String nama = sc04.next();
+String nama = sc04.nextLine();
+nama = sc04.nextLine();
   System.out.print("Masukan baris: ");
 int baris = sc04.nextInt();
   System.out.print("Masukan kolom: ");
 int kolom = sc04.nextInt();
 
-penonton[baris-1][kolom-1] = nama;
+if (baris > 0 && baris <= penonton.length && kolom > 0 && kolom <= penonton[0].length) {
+  penonton[baris - 1][kolom - 1] = nama;
+} else {
+  System.out.println("Nomor baris/kolom tidak tersedia!");
+}
+
+
 
 System.out.print("Input penonton lainnya? (y/n): ");
 String next = sc04.next();
+
 
 if (next.equalsIgnoreCase("n")) {
   break;
