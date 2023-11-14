9ff1794b566c9ad7b27f97a80a2eb847b458d641
Herry <aqsaherryprasetya@gmail.com>
2023-11-14 18:45:14 +0700

    P11_Percobaan2_04

---
+++ src/P11/Square04.java
@@ -0,0 +1,25 @@
+package src.P11;
+
+import java.util.Scanner;
+
+public class Square04 {
+  public static void main(String[] args) {
+    Scanner sc04 = new Scanner(System.in);
+  
+  System.out.print("Masukan nilai N: ");
+  int N = sc04.nextInt();
+  for (int iOuter = 1; iOuter <= N; iOuter++) {
+  
+  for (int i = 0; i <= N; i++) {
+    System.out.print("*");
+  }
+    System.out.println( );
+  }
+
+
+
+
+
+
+  }
+}
