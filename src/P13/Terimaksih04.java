32923a045853f607bb6899200c40ed82dd27d1e4
Herry <aqsaherryprasetya@gmail.com>
2023-11-29 18:44:54 +0700

    P13_Percobaan1_Pertanyaan2_04

--- src/P13/Terimaksih04.java
+++ src/P13/Terimaksih04.java
@@ -3,12 +3,15 @@ package src.P13;
  * Terimaksih04
  */
 public class Terimaksih04 {
-public static void UcapanTerimakasih(){
+// public static void UcapanTerimakasih(){
 
-  System.out.println("Thank you for being the best teacher in the world. \n" + 
+//   System.out.println("Thank you for being the best teacher in the world. \n" + 
+//   "You  inspired in me a love for learnng and made me feel like i  could ask anything.");
+// }
+  public static void main(String[] args) {
+      // UcapanTerimakasih();
+      System.out.println("Thank you for being the best teacher in the world. \n" + 
   "You  inspired in me a love for learnng and made me feel like i  could ask anything.");
 }
-  public static void main(String[] args) {
-      UcapanTerimakasih();
-  }
+  
 }
