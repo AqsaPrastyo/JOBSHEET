d065178a2557d0045aa077f8c519589573e14d80
Herry <aqsaherryprasetya@gmail.com>
2023-11-29 18:40:28 +0700

    P13_Percobaan1_04

---
+++ src/P13/Terimaksih04.java
@@ -0,0 +1,14 @@
+package src.P13; 
+/**
+ * Terimaksih04
+ */
+public class Terimaksih04 {
+public static void UcapanTerimakasih(){
+
+  System.out.println("Thank you for being the best teacher in the world. \n" + 
+  "You  inspired in me a love for learnng and made me feel like i  could ask anything.");
+}
+  public static void main(String[] args) {
+      UcapanTerimakasih();
+  }
+}
---
+++ src/P13/liriklagu.java
@@ -0,0 +1,13 @@
+package src.P13;
+public class liriklagu {
+  public static void main(String[] args) {
+    twinkleMethod();
+    System.out.println("Up above the worl so high \nlike a diamond in the sky");
+    twinkleMethod();
+  }
+  static void twinkleMethod (){
+    System.out.println("Twinkle, Twinkle little star \nHow i wonder what you are");
+    
+  }
+
+}
