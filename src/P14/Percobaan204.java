9eb15b13a66cfc7e0cd534d99bdba6f9d36b411e
Herry <aqsaherryprasetya@gmail.com>
2023-12-06 10:37:51 +0700

    P14_Percobaan2_Pertanyaan2_04

--- src/P14/Percobaan204.java
+++ src/P14/Percobaan204.java
@@ -3,18 +3,27 @@ package src.P14;
 import java.util.Scanner;
 
 public class Percobaan204 {
+  static String deret = " ";
   public static void main(String[] args) {
     Scanner sc04 = new Scanner(System.in);
     System.out.println("Bilangan yang dihitung: ");
     int bilangan = sc04.nextInt();
     System.out.println("Pangkat: ");
     int pangkat = sc04.nextInt();
-    System.out.println(hitungPangkat(bilangan, pangkat));
+
+    int hasil = hitungPangkat(bilangan, pangkat);
+    System.out.println(deret+ " = " + hasil);
+    
   }
 static int hitungPangkat (int x, int y ){
   if (y == 0) {
+    deret += "x1";
     return (1);
   }else {
+    if (!deret.equals("")) {
+      
+    }
+    deret += Integer.toString(x);
     return (x * hitungPangkat(x, y - 1));
   }
 }
